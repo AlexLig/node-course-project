15d7af45c1fa05ffd52f589b55defcbd9a152729
Alextl <alex.trl@hotmail.com>
2018-08-10 01:58:07 +0300

    simple rest api using route

---
+++ api/genres.js
@@ -0,0 +1,158 @@
+const express = require("express")
+const Joi = require("joi");
+
+
+const router = express.Router();
+
+let genres = [
+    { genre: "Comedy", id: 0 },
+    { genre: "Horror", id: 1 },
+    { genre: "Action", id: 2 },
+    { genre: "Mystery", id: 3 }
+];
+
+
+function genreRequestValidation(body) {
+    const schema = {
+        genre: Joi.string().min(3).required()
+    };
+    return Joi.validate(body, schema);
+}
+function findGenre(genre) {
+    return genres.find(g => g.genre.toLowerCase() === genre.toLowerCase())
+}
+
+router.route("/")
+    .get((req, res) => {
+        res.send(genres);
+    })
+    .post((req, res) => {
+        const validation = genreRequestValidation(req.body);
+    
+        if (validation.error) {
+            return res.status(400).send(`400 bad request. ${validation.error.details[0].message}`);
+        }
+        else if (findGenre(req.body.genre)) {
+            return res.status(400).send(`${req.body.genre} genre already exists!`)
+        }
+    
+        let genre = {
+            genre: req.body.genre,
+            id: genres.length + 1 // id goes 3 5 6 7 8... why? => bug!
+        };
+        genres.push(genre);
+        res.send(genre);
+    
+    });
+router.route("/:genre")
+    .get((req, res) => {
+
+        let genreObj = findGenre(req.params.genre);
+        if (!genreObj) {
+            return res.status(404).send("Genre Not Found!");
+        }
+        res.send(genreObj);
+    
+    
+    })
+    .put((req, res) =>{
+        const validation = genreRequestValidation(req.body);
+    
+        if (validation.error) {
+            return res.status(400).send(`400 bad request. ${validation.error.details[0].message}`);
+        }
+        const genre = findGenre(req.params.genre)
+    
+        if(!genre){
+            return res.status(404).send(`404 bad request. ${req.body.genre} genre was not found`);
+        }
+        
+        genre.genre = req.body.genre;
+        res.send(genre);
+    
+    })
+    .delete((req, res) =>{
+
+        const genre = findGenre(req.params.genre)
+    
+        if(!genre){
+            return res.status(404).send(`404 . ${req.params.genre} genre was not found`);
+        }
+        
+        
+        genres.splice(genres.indexOf(genre), 1)
+        res.send(genre);
+    
+    });
+
+
+
+
+// // GET genres
+// router.get("/", (req, res) => {
+//     res.send(genres);
+// });
+// GET genre
+// router.get("/:genre", (req, res) => {
+
+//     let genreObj = findGenre(req.params.genre);
+//     if (!genreObj) {
+//         return res.status(404).send("Genre Not Found!");
+//     }
+//     res.send(genreObj);
+
+
+// });
+
+// // POST genre
+// router.post("/", (req, res) => {
+//     const validation = genreRequestValidation(req.body);
+
+//     if (validation.error) {
+//         return res.status(400).send(`400 bad request. ${validation.error.details[0].message}`);
+//     }
+//     else if (findGenre(req.body.genre)) {
+//         return res.status(400).send(`${req.body.genre} genre already exists!`)
+//     }
+
+//     let genre = {
+//         genre: req.body.genre,
+//         id: genres.length + 1 // id goes 3 5 6 7 8... why? => bug!
+//     };
+//     genres.push(genre);
+//     res.send(genre);
+
+// });
+// UPDATE genre
+// router.put("/:genre", (req, res) =>{
+//     const validation = genreRequestValidation(req.body);
+
+//     if (validation.error) {
+//         return res.status(400).send(`400 bad request. ${validation.error.details[0].message}`);
+//     }
+//     const genre = findGenre(req.params.genre)
+
+//     if(!genre){
+//         return res.status(404).send(`404 bad request. ${req.body.genre} genre was not found`);
+//     }
+    
+//     genre.genre = req.body.genre;
+//     res.send(genre);
+
+// });
+// // DELETE genre
+// router.delete("/:genre", (req, res) =>{
+
+//     const genre = findGenre(req.params.genre)
+
+//     if(!genre){
+//         return res.status(404).send(`404 . ${req.params.genre} genre was not found`);
+//     }
+    
+    
+//     genres.splice(genres.indexOf(genre), 1)
+//     res.send(genre);
+
+// });
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,95 +1,14 @@
-const Joi = require("joi");
+//@ts-check
 const express = require("express");
 const app = express();
+const logger = require("./logger");
+const genres = require("./api/genres");
 
 
 app.use(express.json());
-
-let genres = [
-    { genre: "Comedy", id: 0 },
-    { genre: "Horror", id: 1 },
-    { genre: "Action", id: 2 },
-    { genre: "Mystery", id: 3 }
-];
+app.use(logger);
+app.use("/api/genres", genres);
 
 
-function genreRequestValidation(body) {
-    const schema = {
-        genre: Joi.string().min(3).required()
-    };
-    return Joi.validate(body, schema);
-}
-function findGenre(genre) {
-    return genres.find(g => g.genre.toLowerCase() === genre.toLowerCase())
-}
-
-//GET ITEMS
-app.get("/api/genres", (req, res) => {
-    res.send(genres);
-});
-//GET ITEM
-app.get("/api/genres/:genre", (req, res) => {
-
-    let genreObj = findGenre(req.params.genre);
-    if (!genreObj) {
-        return res.status(404).send("Genre Not Found!");
-    }
-    res.send(genreObj);
-
-
-});
-//POST ITEM
-app.post("/api/genres", (req, res) => {
-    const validation = genreRequestValidation(req.body);
-
-    if (validation.error) {
-        return res.status(400).send(`400 bad request. ${validation.error.details[0].message}`);
-    }
-    else if (findGenre(req.body.genre)) {
-        return res.status(400).send(`${req.body.genre} genre already exists!`)
-    }
-
-    let genre = {
-        genre: req.body.genre,
-        id: genres.length + 1 // id goes 3 5 6 7 8... why? => bug!
-    };
-    genres.push(genre);
-    res.send(genre);
-
-});
-//UPDATE ITEM
-app.put("/api/genres/:genre", (req, res) =>{
-    const validation = genreRequestValidation(req.body);
-
-    if (validation.error) {
-        return res.status(400).send(`400 bad request. ${validation.error.details[0].message}`);
-    }
-    const genre = findGenre(req.params.genre)
-
-    if(!genre){
-        return res.status(404).send(`404 bad request. ${req.body.genre} genre was not found`);
-    }
-    
-    genre.genre = req.body.genre;
-    res.send(genre);
-
-});
-//DELETE ITEM
-app.delete("/api/genres/:genre", (req, res) =>{
-
-    const genre = findGenre(req.params.genre)
-
-    if(!genre){
-        return res.status(404).send(`404 . ${req.params.genre} genre was not found`);
-    }
-    
-    
-    genres.splice(genres.indexOf(genre), 1)
-    res.send(genre);
-
-});
-
-
-
-port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 app.listen(port);
