bd4230c568050ea21e2dff2168933f4a1d3fdae1
Alextl <alex.trl@hotmail.com>
2018-10-11 00:16:22 +0300

    updated api "/:genre" to ":/id". CRUD to mongoDB

--- api/genres.js
+++ api/genres.js
@@ -1,26 +1,47 @@
-const express = require("express")
-const Joi = require("joi");
+const express = require('express');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  }
+}));
+
+// async function getGenre(genreName){
+//   return await Genre.find({genre: genreName})
+// }
+// async function updateGenre(genreName, newName){
+//   const genre = await Genre.find({genreName});
+//   if(!genre) throw new Error('404')
+//   // @ts-ignore
+//   genre.set({name: newName})
+//   // @ts-ignore
+//   genre.save();
+// }
+// async function deleteGenre(genreName){
+//   const result = await Genre.deleteOne({genre: genreName});
+// }
+// async function getGenres(){
+//   return await Genre.find({})
+// }
+// async function createGenre(genreName){
+//   const genre = new Genre({
+//     genre: genreName
+//   })
+//   try{
+//     const result = await genre.save();
+//     return result;
+//   }
+//   catch(ex){
+//     return ex;
+//   }
+// }
 
 const router = express.Router();
-
-let genres = [{
-    genre: "Comedy",
-    id: 0
-  },
-  {
-    genre: "Horror",
-    id: 1
-  },
-  {
-    genre: "Action",
-    id: 2
-  },
-  {
-    genre: "Mystery",
-    id: 3
-  }
-];
-
 
 function genreRequestValidation(body) {
   const schema = {
@@ -29,72 +50,65 @@ function genreRequestValidation(body) {
   return Joi.validate(body, schema);
 }
 
-function findGenre(genre) {
-  return genres.find(g => g.genre.toLowerCase() === genre.toLowerCase())
-}
-
 router.route("/")
-  .get((req, res) => {
+  .get(async (req, res) => {
+    const genres = await Genre.find().sort('name');
     res.send(genres);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     const validation = genreRequestValidation(req.body);
 
     if (validation.error) {
       return res
         .status(400)
         .send(`400 bad request. ${validation.error.details[0].message}`);
-    } else if (findGenre(req.body.genre)) {
-      return res.status(400).send(`${req.body.genre} genre already exists!`)
     }
 
-    let genre = {
-      genre: req.body.genre,
-      id: genres.length + 1 // id goes 3 5 6 7 8... why? => bug!
-    };
-    genres.push(genre);
+    let genre = new Genre({
+      name: req.body.genre
+    });
+
+    genre = await genre.save();
     res.send(genre);
-
   });
-router.route("/:genre")
-  .get((req, res) => {
 
-    let genreObj = findGenre(req.params.genre);
-    if (!genreObj) {
+router.route("/:id")
+  .get(async (req, res) => {
+    const genre = await Genre.findById(req.params.id);
+
+    if (!genre) {
       return res.status(404).send("Genre Not Found!");
     }
-    res.send(genreObj);
 
-
+    res.send(genre);
   })
-  .put((req, res) => {
+  .put(async (req, res) => {
     const validation = genreRequestValidation(req.body);
 
     if (validation.error) {
       return res.status(400).send(`400 bad request. ${validation.error.details[0].message}`);
     }
-    const genre = findGenre(req.params.genre)
+
+    const genre = await Genre.findByIdAndUpdate(req.params.id, {
+      name: req.body.name
+    }, {
+      new: true
+    })
 
     if (!genre) {
-      return res.status(404).send(`404 bad request. ${req.body.genre} genre was not found`);
+      return res.status(404).send(`${req.body.genre} genre was not found`);
     }
 
-    genre.genre = req.body.genre;
     res.send(genre);
-
   })
-  .delete((req, res) => {
-
-    const genre = findGenre(req.params.genre)
+  .delete(async (req, res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id);
 
     if (!genre) {
       return res.status(404).send(`404 . ${req.params.genre} genre was not found`);
     }
 
-
-    genres.splice(genres.indexOf(genre), 1)
     res.send(genre);
-
   });
 
 
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-
+const mongoose = require('mongoose');
 const express = require("express");
 
 const logger = require("./logger");
 const genres = require("./api/genres");
 
 const app = express();
+mongoose.connect("mongodb://localhost/videoClub")
+  .then(() => console.log('\n Connected to MongoDB \n'))
+  .catch(() => console.error('\n Could NOT connect to MongoDB \n'));
 
 app.use(express.json());
 app.use(logger);
