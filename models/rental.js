dbf7000fcef4e568e781c92aa02bc75cbcfd9812
Alextl <alex.trl@hotmail.com>
2018-10-20 18:01:43 +0300

    changes to rental mongoose model

--- models/rental.js
+++ models/rental.js
@@ -6,25 +6,63 @@ const { movieSchema } = require('./movie')
 // rental model
 const Rental = mongoose.model('Rental', new mongoose.Schema({
   customer: {
-    type: customerSchema,
+    // we are creating a new customer schema so we can include only the essential properties instead of all customer document properties
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      },
+      isGold: {
+        type: Boolean,
+        default: false
+      },
+      phone: {
+        type: String,
+        required: true
+      }
+    }),
     required: true
   },
   movie: {
-    type: movieSchema,
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 2,
+        maxlength: 255
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+      }
+
+    }),
     required: true
   },
-  rentDate: {
+  dateOut: {
     type: Date,
-    default: Date.now()
+    required: true,
+    default: Date.now
   },
-  returnDate: Date
+  dateReturned: {
+    type: Date
+  },
+  rentalFee: {
+    type: Number,
+    min: 0
+  }
 }))
 
 // Joi validation
 function validateRental(rental){
   const schema = {
-    customerId: Joi.string().min(2).max(255).required(),
-    movieId: Joi.string().min(2).max(255).required()
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required()
   }
   return Joi.validate(rental, schema)
 }
