1e5fb9911c3371569cc41cd3894600cbf4a762cf
Alextl <alex.trl@hotmail.com>
2018-10-23 19:59:41 +0300

    hashing password

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { User, validate} = require('../models/user')
 const _ = require('lodash')
-
+const bcrypt = require('bcrypt')
 const router = express.Router();
 
 // End Points
@@ -17,6 +17,8 @@ router.route('/')
     }
 
     user = new User(_.pick(req.body, ['name','email', 'password']))
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
     await user.save()
     
     res.send(_.pick(user, ['_id','name','email']))
